0d393f9f6c009d12f70e576c0f22c61f70eaf3cc
Seamus Barnett <seamus0689@gmail.com>
2023-03-14 19:22:30 +0000

    Setup App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Starter from "./pages/Starter";
+import Game from "./pages/Game";
+import End from "./pages/End";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Starter />
+      <Game />
+      <End />
     </div>
   );
 }
