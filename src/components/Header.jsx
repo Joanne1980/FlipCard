54d3e4bcc0ed6075134b294dc35fa4b5e7adb19a
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-15 20:09:45 +0000

    header and navbar component and imported to game page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
+
+
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+  return <>
+  <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-500">
+  <h2 class="mb-4 text-4xl font-leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-blue-900">
+  Flip Card
+  </h2>
+<ul class="flex justify-end">
+  <li class="mr-3">
+    <a class="inline-block border border-blue-900 rounded hover:border-gray-500 text-blue-900 hover:bg-gray-200 py-1 px-3" href="Stats">Stats</a>
+  </li>
+  <li class="mr-3">
+    <a class="inline-block border border-blue-900 rounded hover:border-gray-500 text-blue-900 hover:bg-gray-200 py-1 px-3" href="Instructions">?</a>
+  </li>
+</ul>
+  </nav>
+</>
+  
 }
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import Header from '../components/Header.jsx'
 export default function Game() {
-  return (
+  return <>
+    <Header/>
     <div>Game</div>
-  )
+  
+ </>
 }
