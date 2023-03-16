7b0e1dd909459c781eeab4dbb367d91c5c55a1b7
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-16 19:20:29 +0000

    deleted row file no longer needed

--- src/components/Row.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Row() {
-  return (
-    <div>Row</div>
-  )
-}
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -3,10 +3,9 @@ import React from "react";
 import Grid from "../components/Grid";
 import Header from '../components/Header.jsx'
 export default function Game() {
-  return (
-    <> <Header/>
-
-      <div>Game</div>
+  return ( <>
+    <Header/>
+     <div>Game</div>
       <Grid />
     </>
   );
