0fa5d2199214d2909b2954cbfc1e48dded114399
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-16 18:35:48 +0000

    fixed errors on game page

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
-import React from 'react'
-import Header from '../components/Header.jsx'
-export default function Game() {
-  return <>
-    <Header/>
-    <div>Game</div>
-  
- </>
-=======
+
 import React from "react";
 import Grid from "../components/Grid";
-
+import Header from '../components/Header.jsx'
 export default function Game() {
   return (
-    <>
+    <> <Header/>
+
       <div>Game</div>
       <Grid />
     </>
   );
->>>>>>> 6de8b033e180d290b802f93d031df0dca2ef0d4f
 }
