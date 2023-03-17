692af4634dde68e7f7017587f8ddf385febe38ca
Seamus Barnett <seamus0689@gmail.com>
2023-03-17 19:27:57 +0000

    Image APi grid

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import "./App.css";
 import Game from "./pages/Game";
 import { useState } from "react";
-import Countdown from './components/Countdown'
-
+import Countdown from "./components/Countdown";
 
 function App() {
-
-  const [theme, setTheme] = useState('cats');
+  const [theme, setTheme] = useState("cats");
   const [timer, setTimer] = useState(0);
-  const [cards, setCards] = useState('');
+  const [cards, setCards] = useState("");
   const [correctNumberOfPairs, setCorrectNumberOfPairs] = useState(0);
   const [showInstructions, setShowInstructions] = useState(true);
 
   return (
     <div>
       <Game />
-      <Countdown timer={timer} setTimer={setTimer} />
+      {/* <Countdown timer={timer} setTimer={setTimer} /> */}
     </div>
   );
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ReactBoxFlip from "react-box-flip";
 
 import axios from "axios";
 
@@ -8,39 +9,32 @@ import axios from "axios";
 //Unsplash API Secret key
 //U9QL51xlhb71F5eVTpwuwCJ8tXGOfwQhKq7TWT3HAE8
 
-export default function Grid({theme}) {
+export default function Grid({ theme }) {
   const image = theme;
   const clientId = "CY-iFrJXI04rurx8QvIQCDecckeftZv1kL501Z-hrUw";
 
-  let cards = [];
+  const [isFlipped, setIsFlipped] = useState([]);
+
   const [html, setHtml] = useState([]);
+
+  const [cardsContent, setCardsContent] = useState([]);
+
   useEffect(() => {
+    let cards = [];
+
     axios.get(url).then((response) => {
       for (let i = 0; i < 10; i++) {
         const card = {
           pair: i,
           image: response.data.results[i].urls.small,
           isMatched: "false",
+          isFlipped: false,
         };
         cards.push(card);
         cards.push(card);
       }
-      let deck = [];
-      cards.map((card, i) => {
-        deck.push(
-          <div
-            key={i}
-            data-cardid={i}
-            data-cardpair={card.pair}
-            className="m-3"
-          >
-            <img className="object-cover w-32 h-32" src={card.image} alt="" />
-          </div>
-        );
-      });
-      deck = deck.sort(() => Math.random() - 0.5);
-      setHtml(deck);
-      //console.log(cards);
+      cards = cards.sort(() => Math.random() - 0.5);
+      setCardsContent(cards);
     });
   }, [image]);
 
@@ -48,15 +42,44 @@ export default function Grid({theme}) {
     "https://api.unsplash.com/search/photos?page=1&query=" +
     image +
     "&client_id=" +
-    clientId + 
+    clientId +
     "&orientation=squarish";
+
+  function handleClick(i) {
+    // setIsFlipped(!isFlipped);
+    console.log("click");
+    cardsContent[i].isFlipped = !cardsContent[i].isFlipped;
+    setCardsContent(cardsContent);
+  }
 
   return (
     <>
       <div>Grid</div>
       <div className="flex justify-center">
         <div className="w-full full grid m-0.5 p-0.5 grid-rows-4 grid-cols-4">
-          {html}
+          {cardsContent.map((card, i) => {
+            return (
+              <div
+                key={i}
+                data-cardid={i}
+                data-cardpair={card.pair}
+                className="m-3"
+              >
+                <button onClick={(e) => handleClick(i)}>test button</button>
+                <ReactBoxFlip isFlipped={card.isFlipped}>
+                  <div className="object-cover w-32 h-32">
+                    <img
+                      className="object-cover w-32 h-32"
+                      src={card.image}
+                      alt=""
+                    />
+                  </div>
+
+                  <div className="object-cover w-32 h-32 bg-black"></div>
+                </ReactBoxFlip>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
