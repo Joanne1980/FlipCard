f670473a83132fec0b1305b9478334c399dd4144
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-20 20:34:17 +0000

    adjusting header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 export default function Header() {
   return <>
-  <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-500">
-  <h2 class="mb-4 text-4xl font-leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-blue-900">
+  <nav className="bg-white border-gray-100 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-300 h-15">
+  <h2 class="font-{300} 1.150rem">
   Flip Card
   </h2>
 <ul class="flex justify-end">
