a49364e875a687cb2f4e49f1b36346fd596325d4
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-20 21:11:26 +0000

    adjusting header and added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,16 @@ import Instructions from "./Instructions";
 
 export default function Header() {
   return <>
-  <nav className="bg-white border-gray-100 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-300 h-15">
+  <nav className="px-2 sm:px-4 py-2.5 rounded bg-primary-600 h-15">
   <h2 class="font-{300} 1.150rem">
   Flip Card
   </h2>
+  <div class="flex start">
+    <img
+      alt="game logo"
+      src="././logo1.png"
+    />
+  </div>
 <ul class="flex justify-end">
   <li class="mr-3">
     <a class="inline-block border border-blue-900 rounded hover:border-gray-500 text-blue-900 hover:bg-gray-200 py-1 px-3" href="Stats">Stats</a>
