a7517a9067d420426f6883d646324a79f27a3643
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-20 20:39:55 +0000

    adjusting header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,6 @@ import React, { useState } from "react";
 import Instructions from "./Instructions";
 
 export default function Header() {
-  return <>
-  <nav className="bg-white border-gray-100 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-300 h-15">
-  <h2 class="font-{300} 1.150rem">
-  Flip Card
-  </h2>
-<ul class="flex justify-end">
-  <li class="mr-3">
-    <a class="inline-block border border-blue-900 rounded hover:border-gray-500 text-blue-900 hover:bg-gray-200 py-1 px-3" href="Stats">Stats</a>
-  </li>
-  <li class="mr-3">
-    <a class="inline-block border border-blue-900 rounded hover:border-gray-500 text-blue-900 hover:bg-gray-200 py-1 px-3" href="Instructions">?</a>
-  </li>
-</ul>
-  </nav>
-</>
-  
   return (
     <>
       <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-500">
@@ -46,4 +30,26 @@ export default function Header() {
       </nav>
     </>
   );
+}
+import React from 'react'
+
+
+
+export default function Header() {
+  return <>
+  <nav className="bg-white border-gray-100 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-300 h-15">
+  <h2 class="font-{300} 1.150rem">
+  Flip Card
+  </h2>
+<ul class="flex justify-end">
+  <li class="mr-3">
+    <a class="inline-block border border-blue-900 rounded hover:border-gray-500 text-blue-900 hover:bg-gray-200 py-1 px-3" href="Stats">Stats</a>
+  </li>
+  <li class="mr-3">
+    <a class="inline-block border border-blue-900 rounded hover:border-gray-500 text-blue-900 hover:bg-gray-200 py-1 px-3" href="Instructions">?</a>
+  </li>
+</ul>
+  </nav>
+</>
+  
 }
