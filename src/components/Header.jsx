6051cc40de3b0cf2f6fe2695ac8ffb91c16dd45d
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-21 21:11:34 +0000

    added translate api back in to header but commented out incase we have time to implement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,13 +13,32 @@ export default function Header() {
         </div>
         <ul class="flex justify-end">
           <li class="mr-3">
-            
+            {/* <a
+              class="bg-primary-500 text-primary-200 active:bg-primary-400 font-bold uppercase text-sm px-2 py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button" id="language"
+            >
+              <img alt="Language icon" src="././Lan-icon.png" />
+            </a> */}
           </li>
           <Stats />
         </ul>
       </nav>
     </>
   );  
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://text-translator2.p.rapidapi.com/getLanguages',
+  //   headers: {
+  //     'X-RapidAPI-Key': '6ad8abfd77mshf2eeda7dbb2a73ep178573jsne1cbeaa1df4e',
+  //     'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+  //   }
+  // };
+  
+  // axios.request(options).then(function (response) {
+  //   console.log(response.data);
+  // }).catch(function (error) {
+  //   console.error(error);
+  // });
  
   }
 
