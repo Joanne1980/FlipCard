1ebea961ae75a4d9678c233fe11a445887eb0589
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-21 21:00:24 +0000

    changes made to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,12 +13,7 @@ export default function Header() {
         </div>
         <ul class="flex justify-end">
           <li class="mr-3">
-            <a
-              class="bg-primary-500 text-primary-200 active:bg-primary-400 font-bold uppercase text-sm px-2 py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button" id="language"
-            >
-              <img alt="Language icon" src="././Lan-icon.png" />
-            </a>
+            
           </li>
           <Stats />
         </ul>
@@ -29,27 +24,4 @@ export default function Header() {
   }
 
 
-const encodedParams = new URLSearchParams();
-encodedParams.append("q", "Hello, world!");
-encodedParams.append("target", "es");
-encodedParams.append("source", "en");
-
-const options = {
-  method: 'POST',
-  url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
-  headers: {
-    'content-type': 'application/x-www-form-urlencoded',
-    'Accept-Encoding': 'application/gzip',
-    'X-RapidAPI-Key': '6ad8abfd77mshf2eeda7dbb2a73ep178573jsne1cbeaa1df4e',
-    'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-  },
-  data: encodedParams
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
   
-
