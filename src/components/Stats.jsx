c5929bb0507cde6c7b2ab9947f4c5124e93d11c8
Seamus Barnett <seamus0689@gmail.com>
2023-03-22 01:42:54 +0000

    minor stats change

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -40,6 +40,7 @@ export default function Stats() {
       >
         Highscores
       </button>
+
       {showModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
