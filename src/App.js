1427ff8cabb8533cd1bdb354a3428a46e1650313
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-22 18:54:39 +0000

    commented out some lines for deploy to work, delete if not needed at later date

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Game from "./pages/Game";
 import { useState } from "react";
-import Countdown from "./components/Countdown";
+//import Countdown from "./components/Countdown";
 
 function App() {
-  const [theme, setTheme] = useState("cats");
-  const [timer, setTimer] = useState(0);
-  const [cards, setCards] = useState("");
-  const [correctNumberOfPairs, setCorrectNumberOfPairs] = useState(0);
-  const [showInstructions, setShowInstructions] = useState(true);
+  //const [theme, setTheme] = useState("cats");
+  //const [timer, setTimer] = useState(0);
+  //const [cards, setCards] = useState("");
+  //const [correctNumberOfPairs, setCorrectNumberOfPairs] = useState(0);
+  //const [showInstructions, setShowInstructions] = useState(true);
 
   return (
     <div>
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Grid from "../components/Grid";
 import Header from "../components/Header.jsx";
-import Theme from "../components/Theme";
+//import Theme from "../components/Theme";
 import Instructions from "../components/Instructions";
 
 export default function Game() {
